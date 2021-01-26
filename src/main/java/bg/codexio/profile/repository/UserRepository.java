c1f2ef87583a0f784d0ba3f5f8b7294c06d44b5b
BokLilyan <lilyanivanov96@gmail.com>
2021-01-26 17:30:43 +0200

    I created jpa repository

---
+++ src/main/java/bg/codexio/profile/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package bg.codexio.profile.repository;
+
+import bg.codexio.profile.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
